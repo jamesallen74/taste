4ccbd002791d7b0ab946498068c7c6d4d0a7249c
James Allen <jamesallen74@gmail.com>
2020-09-12 00:29:12 -0400

    Section 4 Repository Pattern

---
+++ Taste.DataAccess/Data/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Taste.Models;
+
+namespace Taste.DataAccess.Data.Repository.IRepository
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        IEnumerable<SelectListItem> GetCategoryListForDropDown();
+
+        void Update(Category category);
+    }
+}
---
+++ Taste.DataAccess/Data/Repository/UnitOfWork.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Taste.DataAccess.Data.Repository.IRepository;
+
+namespace Taste.DataAccess.Data.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public ICategoryRepository Category { get; private set; }
+        
+        private readonly ApplicationDbContext _db;
+
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+            Category = new CategoryRepository(db);
+        }
+
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
